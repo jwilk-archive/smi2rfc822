b64c2f3cd5e258f8c8ca662c188922b330a8211e
Jakub Wilk <jwilk@jwilk.net>
2007-10-03 21:58:03 +0000

    Handle GSM charset correctly.

--- dcs.py
+++ dcs.py
@@ -1,4 +1,5 @@
 from common import read_byte, read_bytes, Reader
+import re
 
 __all__ = ('DataCodingScheme',)
 
@@ -16,7 +17,61 @@ class DataCodingScheme(Reader):
 
 class Scheme7(DataCodingScheme):
 	code = 0
-	_mapping = {}
+	_mapping = \
+	{
+		'\x1b\x0a': u'\u000c',
+		'\x1b\x14': u'\u005e',
+		'\x1b\x28': u'\u007b',
+		'\x1b\x29': u'\u007d',
+		'\x1b\x2f': u'\u005c',
+		'\x1b\x3c': u'\u005b',
+		'\x1b\x3d': u'\u007e',
+		'\x1b\x3e': u'\u005d',
+		'\x1b\x40': u'\u007c',
+		'\x1b\x65': u'\u20ac',
+		'\x00': u'\u0040',
+		'\x01': u'\u00a3',
+		'\x02': u'\u0024',
+		'\x03': u'\u00a5',
+		'\x04': u'\u00e8',
+		'\x05': u'\u00e9',
+		'\x06': u'\u00f9',
+		'\x07': u'\u00ec',
+		'\x08': u'\u00f2',
+		'\x09': u'\u00c7',
+		'\x0b': u'\u00d8',
+		'\x0c': u'\u00f8',
+		'\x0e': u'\u00c5',
+		'\x0f': u'\u00e5',
+		'\x10': u'\u0394',
+		'\x11': u'\u005f',
+		'\x12': u'\u03a6',
+		'\x13': u'\u0393',
+		'\x14': u'\u039b',
+		'\x15': u'\u03a9',
+		'\x16': u'\u03a0',
+		'\x17': u'\u03a8',
+		'\x18': u'\u03a3',
+		'\x19': u'\u0398',
+		'\x1a': u'\u039e',
+		'\x1c': u'\u00c6',
+		'\x1d': u'\u00e6',
+		'\x1e': u'\u00df',
+		'\x1f': u'\u00c9',
+		'\x24': u'\u00a4',
+		'\x40': u'\u00a1',
+		'\x5b': u'\u00c4',
+		'\x5c': u'\u00d6',
+		'\x5d': u'\u00d1',
+		'\x5e': u'\u00dc',
+		'\x5f': u'\u00a7',
+		'\x60': u'\u00bf',
+		'\x7b': u'\u00e4',
+		'\x7c': u'\u00f6',
+		'\x7d': u'\u00f1',
+		'\x7e': u'\u00fc',
+		'\x7f': u'\u00e0',
+	}
 
 	def read(self):
 		nseptets = read_byte(self._file)
@@ -33,7 +88,14 @@ class Scheme7(DataCodingScheme):
 				value >>= 7
 			nbytes -= len(quantum)
 		del output[nseptets:]
-		return ''.join(output)
+		s = ''.join(output)
+		def repl(m):
+			t = m.group(0)
+			try:
+				return self._mapping[t]
+			except KeyError:
+				return t.decode('ISO-8859-1')
+		return re.sub('\x1b.|.', repl, s)
 
 class Scheme8(DataCodingScheme):
 	code = 1
